906dfaef371e64d4ad246e911fea1824f5f7b09a
BenCJones <ben@bencjones.com>
2015-04-07 14:16:30 -0400

    *Adds settings for mac/windows *Corrects rename process

--- tnfChangeImgName.rb
+++ tnfChangeImgName.rb
@@ -1,38 +1,66 @@
 #!/ruby200/bin
 
 require "csv"
+require 'yaml'
 
-csv_file = "C:/Documents and Settings/pos/Desktop/Book1.csv"
-img_dir = "C:/Documents and Settings/pos/My Documents/Downloads/WebAssets"
-img_files = Dir.entries(img_dir)
-img_files.reject! { |item| item !~ /\.jpg$/ }
+#settings for mac and windows
+os = "mac"
+settings = YAML::load_file "tnfChangeImgName.yml"
+csv_file = settings[os]["csv_file"]
+$img_dir = settings[os]["img_dir"]
+Dir.chdir($img_dir) #change to the img directory
 
+#get the ".jpg" files in the image directory
+img_files = Dir.glob('*.jpg')
+#img_files.reject! { |item| item !~ /\.jpg$/ }
+
+#map csv to hashes
 csv_data = CSV.read(csv_file, :headers => true).map { |a| Hash[ (a) ] }
 
+#and discard all with duplicate Desc 2 fields
 csv_data.uniq! { |s| s["Desc 2"] }
 
-
-
-puts img_files
-#puts csv_data
-
-=begin
-class TnfChangeImgName
-	def initialize(csv)
-		@csv = csv
+def rename_file(file, item)
+	if item["Color"]
+		color = "_"+item["Color"].downcase #downcase color name
+		color.gsub!(/[\ \/]/, '_') #replace spaces and slashes with underscores
+		name = "#{item['Style SID']}#{color}.jpg"
+	else
+		name = "#{item['Style SID']}.jpg"
 	end
-
-	def list_files(dir)
-	
+	if File.rename($img_dir+file, $img_dir+name)
+#		puts name
+	else
+		puts "Error: #{name}"
 	end
-	
-	def change_file_name(file, name)
-	
-	end
-
-	csv_list = import(@csv) do
-		read_attribute_from_file
-	end
-
 end
-=end
+
+def rename_test(file, item)
+	if item["Color"]
+		color = "_"+item["Color"].downcase #downcase color name
+		color.gsub!(/[\ \/]/, '_') #replace spaces and slashes with underscores
+		name = "#{item['Style SID']}#{color}.jpg"
+	else
+		name = "#{item['Style SID']}.jpg"
+	end
+	puts "#{file} becomes #{name}"
+end
+
+csv_data.each do |item|
+	style = item["Desc 2"]
+	if style.length > 4
+		style.gsub!(/[\ \-\/\\]/, "_") #replace space, slash, dash with underscore
+		file = Dir.glob("*#{style}*")
+		if file[0].nil?
+			puts "Failure with file #{style}"
+		else
+			rename_file(file[0], item)
+		end
+	else
+		if file.nil?
+			puts "Failure with file #{style}"
+		else
+			rename_test(file, item)
+		end
+	end
+end
