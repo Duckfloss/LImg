10be37497aa727de277d03823c1357ce74b83ca7
BenCJones <ben@bencjones.com>
2015-12-09 14:08:02 -0500

    Working CLI script for image resizing

--- limg.rb
+++ limg.rb
@@ -4,21 +4,20 @@
 
 require 'optparse'
 require 'ostruct'
-#require 'RMagick'
-
+require 'RMagick'
+include Magick
 
 
 class Parser
 
-	FORMATS = ["all","t","thumb","thumbnail","med","medium","lg","large","sw","swatch"]
+	FORMATS = ["all","t","med","lg","sw"]
 
 	def self.parse(args)
 		options = OpenStruct.new
-		options.format = "all"
+		options.format = ["all"]
 		options.eci = false
 		options.source = nil
 		options.dest = nil
-		options.size = nil
 		options.verbose = false
 
 		opt_parser = OptionParser.new do |opt|
@@ -26,13 +25,9 @@ class Parser
 			opt.separator ""
 			opt.separator "Options:"
 
-			opt.on("-e", "--eci", "Only parse pic(s) to ECI's image directory") do
-				options.eci = true
-			end
-
-			opt.on("--source SOURCE", "Sets source file or directory") do |source|
+			opt.on("--source SOURCE", "Sets source file or directory", "  default is Downloads/WebAssets") do |source|
 				# Validate source
-				if !Dir.exist?(source)
+				if !File.directory?(source)
 					if File.exist?(source)
 						options.source = { "file"=>source }
 					end
@@ -45,7 +40,9 @@ class Parser
 				end
 			end
 
-			opt.on("--dest DEST", "Sets destination directory") do |dest|
+			opt.separator ""
+
+			opt.on("--dest DEST", "Sets destination directory", "  defaults are R:/RETAIL/IMAGES/4Web", "  and R:/RETAIL/RPRO/Images/Inven") do |dest|
 				if !Dir.exist?(dest)
 					puts "error" #error
 				else
@@ -53,32 +50,31 @@ class Parser
 				end
 			end
 
-			opt.on("--size WXH", "Sets custom output size", "  format should be similar to \"125x125\"") do |size|
-				splitter = size.downcase.index('x')
-				if splitter.nil?
-					puts "error" #error
-				end
-				options.size = size.split('x').each do |i|
-					if i.to_i < 0
+			opt.separator ""
+
+			opt.on("-e", "--eci", "Parses pic(s) to ECI's directory", "  as well as to default or selected destination") do
+				options.eci = true
+			end
+
+			opt.separator ""
+
+			opt.on("-fFORMAT", "--format FORMAT", Array, "Select output formats", "  accepts comma-separated string", "  output sizes are t,sw,med,lg", "  default is \"all\"") do |formats|
+				formats.each do |format|
+					if FORMATS.index(format.downcase).nil?
 						puts "error" #error
+						exit
 					end
+					options.format = formats
 				end
 			end
 
-			opt.on("-fFORMAT", "--format FORMAT", "Select a single output format", "  output sizes are t, sw, med, and lg", "  default is \"all\"") do |format|
-				if FORMATS.index(format.downcase).nil?
-					puts "error" #error
-				else
-					options.format = format.downcase
-				end
-			end
+			opt.separator ""
 
-			opt.on("-v", "--verbose", "Run chattily (or not)") do |v|
+			opt.on("-v", "--verbose", "Run chattily (or not)", "  default runs not verbosely") do |v|
 				options.verbose = true
 			end
 
 			opt.separator ""
-			opt.separator "Also:"
 
 			opt.on_tail("-h","--help","Prints this help") do
 				puts opt
@@ -93,9 +89,166 @@ end
 
 class Image_Chopper
 
-	def initialize(*args)
-		
+	FORMATS = {
+		"lg"=>1050,
+		"med"=>350,
+		"sw"=>350, # this is temporary til I figure out how to automate
+		"t"=>100
+	}
+
+	def initialize(options)
+		# Default destination directory
+		dest = "R:/RETAIL/IMAGES/4Web"
+		eci = "R:/RETAIL/RPRO/Images/Inven"
+		# basearray lets us avoid duplicate output
+		$basearray = []
+
+		# SOURCE
+		if options.source.nil? 
+			options.source = { "dir" => "C:/Documents and Settings/pos/My Documents/Downloads/WebAssets" }
+		end
+		if options.source.key?("dir")
+			images = Dir.entries(options.source["dir"])
+			images.keep_if { |file| file =~ /\.jpg$|\.png$|\.jpeg$|\.gif$/ }
+		elsif options.source.key?("file")
+			images = [ options.source["file"] ]
+		else
+			puts "error" #error
+		end
+
+		# Reporting
+		if options.verbose
+			puts "Source: #{options.source.values[0]}\n"
+		end
+
+		# Image processing loop
+		images.each do |image|
+			# register output array
+			$outputs = []
+
+			# Parse filename
+			filename = image.slice(File.dirname(image).length+1..-1)
+			filebase = filename.slice(/^[A-Z]{16}/)
+			fileattr = filename.slice(/(?<=\_)([A-Za-z0-9\_]+)(?=\.)/)
+			if !$basearray.include?(filebase)
+				$basearray << filebase
+			end
+
+			# DESTINATION
+			if options.dest
+				dest = options.dest
+			end
+			# Reporting
+			if options.verbose
+				puts "Destination: #{dest}\n"
+			end
+
+			# Begin reporting
+			$report = "\t#{filename}"
+
+			# ECI
+			if options.eci
+				# Reporting
+				if options.verbose
+					puts "ECI parsing is on\n"
+				end
+				if !$basearray.include?(filebase)
+					$report << "\n\t  exists in ECI directory"
+					break
+				else
+					$outputs << { size: 1050, dest: dest, name: "#{filebase}_lg.jpg" }
+					$outputs << { size: 350, dest: eci, name: "#{filebase}.jpg" }
+					$outputs << { size: 100, dest: eci, name: "#{filebase}t.jpg" }
+				end
+			end
+
+			# FORMAT
+			if options.format
+				if options.format.include?("all")
+					options.format = FORMATS.keys
+				end
+				options.format.each do |format|
+					$outputs << { size: FORMATS[format], dest: dest, name: "#{filebase}_#{fileattr}_#{format}.jpg" }
+				end
+			end # format loop
+
+			# Sort the $outputs array by size
+			$outputs.sort! { |x,y| x[:size] <=> y[:size] }
+			$outputs.reverse!
+
+			# Preformat imgage
+			image = preformat(image)
+			# Chop up images
+			$outputs.each do |output|
+				ximg = resize( image,output[:size] )
+				$report << "\n\t  Resized to #{output[:size]}x#{output[:size]}"
+				write_file(ximg, "#{output[:dest]}/#{output[:name]}")
+				$report << "\n\t  Saved as: #{output[:name]}"
+			end
+
+			# Reporting
+			if options.verbose
+				puts "\t################################\n"
+				puts $report << "\n"
+			end
+		end # image loop
 	end
+
+	def preformat(img)
+		# Create new image object and set defaults
+		cat = ImageList.new($test) do
+			self.background_color = "#ffffff"
+			self.gravity = CenterGravity
+		end
+
+		# If the image is CMYK, change it to RGB
+		color_profile = "C:/WINDOWS/system32/spool/drivers/color/sRGB Color Space Profile.icm"
+		if cat.colorspace == Magick::CMYKColorspace
+			$report << "\n\t  Colors: #{cat.colorspace} -> #{color_profile}"
+			cat = cat.add_profile(color_profile)
+		end
+
+		# If the image has alpha channel transparency, fill it with background color
+		if cat.alpha?
+			$report << "\n\t  Alpha: Transparent -> #{cat.background_color}"
+			cat.alpha(BackgroundAlphaChannel)
+		end
+
+		# If the image size isn't a square, make it a square
+		img_w = cat.columns
+		img_h = cat.rows
+		ratio = img_w.to_f/img_h.to_f
+		if ratio < 1
+			$report << "\n\t  Size: #{img_w}x#{img_h} -> #{img_h}x#{img_h}"
+			x = img_h/2-img_w/2
+			cat = cat.extent(img_h,img_h,x=-x,y=0)
+		elsif ratio > 1
+			$report << "\n\t  Size: #{img_w}x#{img_h} -> #{img_w}x#{img_w}"
+			y = img_w/2-img_h/2
+			cat = cat.extent(img_w,img_w,x=0,y=-y)
+		end
+		cat
+	end
+
+	def resize(cat,size)
+		cat = cat.resize(size,size)
+	end
+
+	def write_file(cat,dest)
+		cat.write(dest) do
+			self.quality = 80
+			self.density = "72x72"
+		end
+	end
+
+end
+
+$test = "C:/Documents and Settings/pos/Desktop/test/in/PAIEADNFAGBOMIOE_gray.png"
+$tgt = "C:/Documents and Settings/pos/Desktop/test/in/PAIEADNFAGBOMIOE_grayOUT.jpg"
+
+
+if File.exist?($tgt)
+	File.delete($tgt)
 end
 
 
@@ -103,8 +256,7 @@ if __FILE__ == $0
 
 options = Parser.parse(ARGV)
 puts options.to_h
-
-
+Image_Chopper.new(options)
 
 end
 
